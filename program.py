1908d9adb8ff8b2861cbf005e699df5825173439
Santicrek <ssantiago.acevedo.7@gmail.com>
2024-10-24 19:05:04 -0500

    wiki v0.9

--- program.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.shortcuts import render
-from django.http import HttpResponse
-from . import views
-
-def index(request):
-    return HttpResponse("Hello!")
-
